d9075ae8195982b9273466b970d5fe56ba701fbe
Emilio Pardo <e.pardo.perez@gmail.com>
2023-02-08 14:23:50 +0100

    se crea dist

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import M$plugin$LayerSelector from '/home/emilio/proyectos/ieca/layerselector/src/facade/js/layerselector';
+import M$control$LayerSelectorControl from '/home/emilio/proyectos/ieca/layerselector/src/facade/js/layerselectorcontrol';
+import M$impl$control$LayerSelectorControl from '/home/emilio/proyectos/ieca/layerselector/src/impl/ol/js/layerselectorcontrol';
+
+if (!window.M.plugin) window.M.plugin = {};
+if (!window.M.control) window.M.control = {};
+if (!window.M.impl) window.M.impl = {};
+if (!window.M.impl.control) window.M.impl.control = {};
+window.M.plugin.LayerSelector = M$plugin$LayerSelector;
+window.M.control.LayerSelectorControl = M$control$LayerSelectorControl;
+window.M.impl.control.LayerSelectorControl = M$impl$control$LayerSelectorControl;
